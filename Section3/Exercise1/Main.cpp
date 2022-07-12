9acb4cad444c5784064a60683dc411aaeb1e1dbb
camodesigns <camilo970403@gmail.com>
2022-07-12 16:36:12 -0500

    Exercise1 Update

--- Section3/Exercise1/Main.cpp
+++ Section3/Exercise1/Main.cpp
@@ -7,12 +7,18 @@ int main() {
 	int Number;
 	cout << " Please enter a number between 1-10 please" << endl;
 	cin >> Number;
+	int Contador = 0;
 	while (Number > 10 || Number < 1 || (!(cin >> Number))) {
 		cout << "Invalid Input, try again! " << endl;;
 		cin.clear();
 		cin.ignore(1000, '\n');
 		cout << "Please enter a number between 1-10:" << endl;
 		cin >> Number;
+		Contador += 1;
+		cout << Contador << endl;
+		if (Contador >5) {
+			cout<<"No more think loser"<<endl;
+		}
 		cout << "Your Number is:" << Number << endl;
 	}
 	return 0;
