780e40c7f965aac593752f822151036db9e1256d
camodesigns <camilo970403@gmail.com>
2022-07-18 10:16:54 -0500

    Week1-Section3- Exercise 3 and 4 update
    position brackets changed
    Exercise 4 Logic changed for following the exercise rules

--- Section3/Exercise4/Main4.cpp
+++ Section3/Exercise4/Main4.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
 	int sum = 0;
 	cout << "Multiples of 10 " << "[";
-	for (int i = 0; i < 101; i++) {
+	for (int i = 10; i < 101; i+=10) {
 		if (i % 10 == 0) {
 			cout << i<<",";	
 		}
