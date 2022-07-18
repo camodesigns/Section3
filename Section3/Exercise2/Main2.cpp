7cc63e60d63ab40eeb2b582b87e7be1d817ec3c0
camodesigns <camilo970403@gmail.com>
2022-07-18 09:21:29 -0500

    Week1-Section3-Exercise 2 Updates
    Input Verification
    Useless comments eliminated

--- Section3/Exercise2/Main2.cpp
+++ Section3/Exercise2/Main2.cpp
@@ -7,17 +7,18 @@ using namespace std;
 int main() {
 	char Answer1, Answer2, Answer3;
 	cout << "Welcome to your quiz!" << endl;
-	cout << "F-False" << endl;//0
-	cout << "T-True" << endl;//1
+	cout << "F-False" << endl;
+	cout << "T-True" << endl;
 	int Counter = 0;
 	int FaultCounter = 0,FaultCounter2 = 0, FaultCounter3 = 0;
 	float Score = 0.0f;
 	cout << "Question 1(1pt)" << endl;
 	cout << "1. The meat is fruit?" << endl;
 	cin >> Answer1;
-	while(Answer1!='T'&& Answer1!='F') 
+	while(cin.good()|| Answer1!='T'&& Answer1!='F') 
 	{
-		if(FaultCounter <5){
+		if(FaultCounter <5)
+		{
 			cout << "Invalid Input, try again! " << endl;
 		cin.clear();
 		cin.ignore(1000, '\n');
@@ -44,7 +45,8 @@ int main() {
 	cout << "Question 2(2pts)" << endl;
 	cout << "2.The human is composed for 80% water?" << endl;
 	cin >> Answer2;
-	while (Answer2 != 'T' && Answer2 != 'F') {
+	while (cin.good() || Answer2 != 'T' && Answer2 != 'F')
+	{
 		if (FaultCounter2 < 5) {
 			cout << "Invalid Input, try again! " << endl;
 			cin.clear();
@@ -73,7 +75,8 @@ int main() {
 	cout << "Question 3(2pts)" << endl;
 	cout << "3. Teravision Games is a Shoes Company" << endl;
 	cin >> Answer3;
-	while (Answer3 != 'T' && Answer3 != 'F') {
+	while (cin.good() || Answer3 != 'T' && Answer3 != 'F')
+	{
 		if (FaultCounter3 < 5) {
 			cout << "Invalid Input, try again! " << endl;
 			cin.clear();
